c551a05d70a820403f6009f1bd18a2a41f6f831d
Allen Ng <sju.akng@gmail.com>
2017-04-21 23:34:12 -0500

    Webpack files and added event api

--- src/main/java/com/thriveng/dao/EventDao.java
+++ src/main/java/com/thriveng/dao/EventDao.java
@@ -1,7 +1,45 @@
 package com.thriveng.dao;
 
+import com.thriveng.domain.Task;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by DoctorSushi on 4/15/17.
  */
 public class EventDao {
+
+    List<Task> taskList;
+    String eventName;
+    Date eventDate;
+
+    public EventDao(String eventName, Date eventDate) {
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+    }
+
+    public List<Task> getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(List<Task> taskList) {
+        this.taskList = taskList;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
 }
--- src/main/java/com/thriveng/rest/EventController.java
+++ src/main/java/com/thriveng/rest/EventController.java
@@ -1,10 +1,27 @@
 package com.thriveng.rest;
 
+import com.thriveng.dao.EventDao;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by DoctorSushi on 4/15/17.
  */
 @Controller
+@RequestMapping("thriveng-service/events")
 public class EventController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<EventDao> getEvents() {
+        EventDao sampleEvent = new EventDao("Allen and Jenny's Wedding", new Date("11/02/2012"));
+        List<EventDao> events = new ArrayList<>();
+        events.add(sampleEvent);
+        return events;
+    }
 }
